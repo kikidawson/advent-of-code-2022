b3dcd111d64d7f3ec9fa6887819f56c32a7e7079
kikidawson <kikiadawson@gmail.com>
2022-12-02 19:21:25 +0000

    part 2 - day 2

--- 02/main.js
+++ 02/main.js
@@ -1,15 +1,15 @@
 const fs = require("fs");
 
 const points = {
-  "A X": 4,
-  "A Y": 8,
-  "A Z": 3,
+  "A X": 3,
+  "A Y": 4,
+  "A Z": 8,
   "B X": 1,
   "B Y": 5,
   "B Z": 9,
-  "C X": 7,
-  "C Y": 2,
-  "C Z": 6
+  "C X": 2,
+  "C Y": 6,
+  "C Z": 7
 }
 
 var total = 0;
@@ -30,12 +30,12 @@ fs.readFile("./data.csv", "utf8", function (err, data) {
 
 //    1  |  2  | points | extra |  total  |
 //   ______________________________________
-//    A  |  X  |   3    |   1   |   4
-//    A  |  Y  |   6    |   2   |   8
-//    A  |  Z  |   0    |   3   |   3
+//    A  |  X  |   0    |   3   |   3
+//    A  |  Y  |   3    |   1   |   4
+//    A  |  Z  |   6    |   2   |   8
 //    B  |  X  |   0    |   1   |   1
 //    B  |  Y  |   3    |   2   |   5
 //    B  |  Z  |   6    |   3   |   9
-//    C  |  X  |   6    |   1   |   7
-//    C  |  Y  |   0    |   2   |   2
-//    C  |  Z  |   3    |   3   |   6
+//    C  |  X  |   0    |   2   |   2
+//    C  |  Y  |   3    |   3   |   6
+//    C  |  Z  |   6    |   1   |   7
