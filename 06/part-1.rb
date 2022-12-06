370e25c1eec56e5302fdffd01332e61740a45948
kikidawson <kikiadawson@gmail.com>
2022-12-06 18:31:54 +0000

    day 6

---
+++ 06/part-1.rb
@@ -0,0 +1,13 @@
+data   = File.open("data.txt")
+packet = data.read.chars
+
+def repetition(array)
+  array.uniq.length == array.length
+end
+
+for i in 3..packet.length - 1 do
+	if repetition(packet[i-3..i]) then
+    p "start-of-packet marker = #{i + 1}"
+		break
+	end
+end
